c11ad0beee6d55e5fb0ea12dd58e660d0d6d9724
Pascal ECHEMANN <pechemann@gmail.com>
2019-08-19 19:20:41 +0200

    Test update
    Add test suite for the HyperionBaseProcessType class.

--- test/com/asteria/hyperion/validator/AbstractHyperionValidatorTest.ts
+++ test/com/asteria/hyperion/validator/AbstractHyperionValidatorTest.ts
@@ -2,10 +2,10 @@ import 'mocha';
 import { expect } from 'chai';
 
 // Class to test:
-import { AbstractHyperionValidator } from '../../../../../../src/com/asteria/hyperion/validator/AbstractHyperionValidator';
+import { AbstractHyperionValidator } from '../../../../../src/com/asteria/hyperion/validator/AbstractHyperionValidator';
 
 // Utilities:
-import * as utils from '../../../../../../utils/test-utils/classes/AbstractHyperionValidatorImpl';
+import * as utils from '../../../../../utils/test-utils/classes/AbstractHyperionValidatorImpl';
 
 // Test:
 describe('AbstractHyperionValidator class test', ()=> {
---
+++ utils/test-utils/utilities/HyperionBaseProcessTypeTestUtils.ts
@@ -0,0 +1,13 @@
+/*!
+ * This module constains utilities used by the HyperionBaseProcessType test  suite.
+ */
+
+// Utilities:
+// We store constants that should be defined by the HyperionBaseProcessType class:
+export const READ_FILE: string = 'read-file';
+export const WRITE_FILE: string = 'write-file';
+export const CSV_TO_LIST: string = 'csv-to-list';
+export const LINES_TO_LIST: string = 'lines-to-list';
+export const LIST_TO_CSV: string = 'list-to-csv';
+export const FILTER: string = 'filter';
+export const CSV_PREVIEW: string = 'csv-preview';
